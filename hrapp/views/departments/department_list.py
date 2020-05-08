0cef9cb51387fc65dce134aa7175913c0de22168
michaelprince3 <michaelprince3@gmail.com>
2020-05-08 15:58:56 -0500

    initial department list

---
+++ hrapp/views/departments/department_list.py
@@ -0,0 +1,43 @@
+import sqlite3
+from django.shortcuts import render
+from hrapp.models import Employee
+from ..connection import Connection
+
+
+def employee_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            # TODO: Add to query: e.department,
+            db_cursor.execute("""
+            select
+                e.id,
+                e.first_name,
+                e.last_name,
+                e.start_date,
+                e.is_supervisor
+            from hrapp_employee e
+            """)
+
+            all_employees = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                employee = Employee()
+                employee.id = row['id']
+                employee.first_name = row['first_name']
+                employee.last_name = row['last_name']
+                employee.start_date = row['start_date']
+                employee.is_supervisor = row['is_supervisor']
+                # employee.department = row['department']
+
+                all_employees.append(employee)
+
+    template = 'employees/employees_list.html'
+    context = {
+        'employees': all_employees
+    }
+
+    return render(request, template, context)
