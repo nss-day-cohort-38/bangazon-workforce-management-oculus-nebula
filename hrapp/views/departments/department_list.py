c85d92f07de98458b5de1f460316282e85a1a766
michaelprince3 <michaelprince3@gmail.com>
2020-05-11 09:38:47 -0500

    more departments

--- hrapp/views/departments/department_list.py
+++ hrapp/views/departments/department_list.py
@@ -1,39 +1,28 @@
 import sqlite3
 from django.shortcuts import render
-from hrapp.models import Employee
+from hrapp.models import Department, model_factory
 from ..connection import Connection
 
 
-def employee_list(request):
+def department_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
-            conn.row_factory = sqlite3.Row
+            conn.row_factory = model_factory(Department)
             db_cursor = conn.cursor()
 
-            # TODO: Add to query: e.department,
             db_cursor.execute("""
             select
-                e.id,
-                e.first_name,
-                e.last_name,
+                d.id,
+                d.department_name,
+                d.budget,
                 e.start_date,
                 e.is_supervisor
-            from hrapp_employee e
+            from hrapp_department d
             """)
 
-            all_employees = []
-            dataset = db_cursor.fetchall()
+            all_departments = db_cursor.fetchall()
 
-            for row in dataset:
-                employee = Employee()
-                employee.id = row['id']
-                employee.first_name = row['first_name']
-                employee.last_name = row['last_name']
-                employee.start_date = row['start_date']
-                employee.is_supervisor = row['is_supervisor']
-                # employee.department = row['department']
 
-                all_employees.append(employee)
 
     template = 'employees/employees_list.html'
     context = {
