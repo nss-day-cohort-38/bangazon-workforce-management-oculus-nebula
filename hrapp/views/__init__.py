602767351e0ec751c45d082abf1d5c38b438061b
Matt Crook <matt.crook11@gmail.com>
2020-05-12 09:42:47 -0500

    fix: deleted JS files. code clean up. COmmented out attendees list. Got rid of details button on TrainingProgram list.

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -4,7 +4,7 @@ from .training_programs.training_programs_list import training_programs_list
 from .training_programs.training_program_details import training_program_details
 from .training_programs.training_program_form import training_program_form
 from .training_programs.training_program_form import training_program_edit_form
-from .training_programs.training_programs_attendees import employee_attendees
+# from .training_programs.training_programs_attendees import employee_attendees
 from .computers import computer_list
 from .computers import computer_list, computer_details
 from .computers import computer_list, computer_details, computer_form, confirm_computer_delete
