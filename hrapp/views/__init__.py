0570c6265c711e798dd2109d6136ad1c5047c1b7
michaelprince3 <michaelprince3@gmail.com>
2020-05-12 10:14:45 -0500

    details complete

--- hrapp/views/__init__.py
+++ hrapp/views/__init__.py
@@ -2,5 +2,5 @@ from .employees.employee_list import employee_list
 from .employees.employee_details import employee_details
 from .home import home
 from .auth.logout import logout_user
-from .departments import department_list, department_details
+from .departments import department_list, department_detail
 from .computers import computer_list, computer_details, computer_form
--- hrapp/views/departments/department_details.py
+++ hrapp/views/departments/department_details.py
@@ -5,22 +5,51 @@ from hrapp.models import Department, Employee
 from hrapp.models import model_factory
 from ..connection import Connection
 
-def create_department(cursor, row):
-    _row = sqlite3.Row(cursor, row)
+# def create_department(cursor, row):
+#     _row = sqlite3.Row(cursor, row)
     
-    department = Department()
-    department.id = row["id"]
-    department.department_name = row["department_name"]
-    department.budget = row["budget"]
+#     department = Department()
+#     department.id = row["id"]
+#     department.department_name = row["department_name"]
+#     department.budget = row["budget"]
     
-    department.employees = []
+#     department.employees = []
     
-    employee = Employee()
-    employee.id = row["id"]
-    employee.first_name = row["first_name"]
-    employee.last_name = row["last_name"]
+#     employee = Employee()
+#     employee.id = row["id"]
+#     employee.first_name = row["first_name"]
+#     employee.last_name = row["last_name"]
     
-    return (department, employee,)
+#     return (department, employee,)
+
+def get_employees(department_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+            SELECT
+            e.id,
+            e.first_name,
+            e.last_name,
+            e.start_date,
+            e.department_id
+            FROM hrapp_employee e
+            WHERE e.department_id = ?                          
+            """, (department_id,))
+        
+        dep_employees = []
+        data = db_cursor.fetchall()
+        
+        for row in data:
+            employee = Employee()
+            employee.id = row['id']
+            employee.first_name = row['first_name']
+            employee.last_name = row['last_name']
+            
+            dep_employees.append(employee)
+        
+        return dep_employees
 
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
@@ -41,8 +70,12 @@ def get_department(department_id):
 def department_detail(request, department_id):
     if request.method == 'GET':
         department = get_department(department_id)
+        employees = get_employees(department_id)
         
-        template = 'departments/details'
+        template = 'departments/department_details.html'
         context = {
-            'department' : department
+            'department' : department,
+            'employees' : employees
         }
+        
+        return render(request, template, context)
