852785321eba63176312d9d9b6440546bde8fc4f
Trinity Terry <trinityterry@Trinitys-MacBook-Pro.local>
2020-05-13 14:34:54 -0500

    added no max capacity option

---
+++ hrapp/views/employees/data_manager/programs/current_capacity.py
@@ -0,0 +1,20 @@
+import sqlite3
+from hrapp.views.connection import Connection
+from hrapp.models import model_factory, TrainingProgramEmployee
+def current_capacity(program_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(TrainingProgramEmployee)
+
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            Count(*) count
+        FROM
+            hrapp_trainingprogramemployee tre
+        WHERE
+            tre.training_program_id = ?
+        GROUP BY training_program_id
+        """, (program_id, ))
+
+        return db_cursor.fetchone().count 
